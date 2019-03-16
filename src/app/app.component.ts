5327dd79dd7b4597b3d754947ab964984b917737
Sébastien Conejo <sebastien.conejo@buddyweb.fr>
2019-03-16 17:51:56 +0100

    Style default panel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['../assets/styles/main.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent {
-  title = 'information-panel';
+  title = 'information-panel'
 }
